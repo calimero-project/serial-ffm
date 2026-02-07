ca2b2c65c8c514089dcdd6ec679c4ad5c29fd8e9
bmalinowsky <bmalinowsky@github.com>
2026-02-07 12:07:06 +0100

    Upgrade to proguard-gradle 7.8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.util.Locale
 
 buildscript {
     dependencies {
-        classpath("com.guardsquare:proguard-gradle:7.8.1")
+        classpath("com.guardsquare:proguard-gradle:7.8.2")
     }
 }
 
